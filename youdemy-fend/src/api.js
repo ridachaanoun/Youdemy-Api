476a484b2bfb3a3b2109ed9699694033e0935e8c
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-01-19 16:24:05 +0100

    Added automatic Authorization header in api.js using token from cookies for authenticated API requests.

--- youdemy-fend/src/api.js
+++ youdemy-fend/src/api.js
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: 'http://localhost:8000/api',
 });
 
+// Add Authorization header if token exists in cookies
+api.interceptors.request.use(config => {
+  const token = document.cookie.split('; ').find(row => row.startsWith('api_key='))?.split('=')[1];
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
