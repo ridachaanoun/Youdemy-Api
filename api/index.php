94dbc5bcdf2748b9f2d95f8a7f7393bcb0600b0a
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-01-19 14:19:12 +0100

    Fix CORS issue by handling preflight OPTIONS requests in backend

--- api/index.php
+++ api/index.php
@@ -6,6 +6,9 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 // Includes
 require_once 'Database.php';
 require_once 'controllers/UserController.php';
@@ -15,6 +18,12 @@ require_once 'controllers/StudentController.php';
 require_once 'controllers/TeacherController.php';
 require_once 'controllers/AdministratorController.php';
 require_once 'routes/Router.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    echo "reda";
+    exit();
+}
 
 $database = new Database();
 $db = $database->connect();
